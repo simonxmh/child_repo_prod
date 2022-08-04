f59b8bb2d09a9867c60ea4cd4567acde54bf31b5
simonxmh <simh@live.ca>
2022-08-04 15:55:58 -0400

    add terraform provider in child repo"

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    tfe = {
+      version  = "~> 0.35.0"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname = var.hostname
+}
+
 resource "tfe_workspace" "child" {
   organization = var.organization
   name = "child"
