7e5229dc0eb526535aa448ce4d747768e9d72788
simonxmh <simh@live.ca>
2022-08-04 17:11:25 -0400

    remove them all

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    tfe = {
-      version  = "~> 0.35.0"
-    }
-  }
-}
-
-provider "tfe" {
-  hostname = var.hostname
-  token = var.TF_TOKEN_simontest_ngrok_io
-}
-
 resource "tfe_workspace" "child" {
   organization = var.organization
   name = "child"
